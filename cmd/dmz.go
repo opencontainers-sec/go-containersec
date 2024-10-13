b7d7d6f1505c5ed6523e1db312e219f83b6909ab
lifubang <lifubang@acmcoder.com>
2024-10-13 21:37:32 +0800

    fmt.Printf does not support error-wrapping directive %w
    Signed-off-by: lifubang <lifubang@acmcoder.com>

--- cmd/dmz.go
+++ cmd/dmz.go
@@ -14,7 +14,7 @@ func main() {
 	if wd, err := unix.Getwd(); errors.Is(err, unix.ENOENT) {
 		fmt.Println("current working directory is outside of container mount namespace root -- possible container breakout detected")
 	} else if err != nil {
-		fmt.Printf("failed to verify if current working directory is safe: %w\n", err)
+		fmt.Printf("failed to verify if current working directory is safe: %v\n", err)
 	} else if !filepath.IsAbs(wd) {
 		// We shouldn't ever hit this, but check just in case.
 		fmt.Printf("current working directory is not absolute -- possible container breakout detected: cwd is %q\n", wd)
